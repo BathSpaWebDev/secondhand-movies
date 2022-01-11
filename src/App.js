719ae33b0aaeca5c871ba69f7c1b21af84bf6d40
abulyousuf <abulyousuf@btinternet.com>
2022-01-11 04:18:23 +0000

    Access data

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+  const [moviesData, setMoviesData] = useState([]);
+
+  const getData = () => {
+    fetch("movies.json", {
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+    })
+      .then(response => response.json())
+      .then(data => setMoviesData(data));
+  };
+
+  console.log(moviesData);
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  return <div></div>;
+};
 
 export default App;
