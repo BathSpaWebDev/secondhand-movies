f1437f30daf61c138ef1959dd36fbbe35a9c151d
Kerry Clements <kerryclements@Kerrys-Air.local.tld>
2022-01-12 14:44:29 +0000

    navigation bar initial commit

---
+++ src/components/Navigation.jsx
@@ -0,0 +1,11 @@
+import "./Navigation.css"
+
+export const NavBar = () =>{
+    return(
+        <nav className="box">
+            <h3>Logo</h3>
+            <h3>Heading</h3>
+            <h3>Basket</h3>
+        </nav>
+    );
+}
