b77577a7ddcf78e1b7304b1243e75fabb652e338
abulyousuf <abulyousuf@btinternet.com>
2022-01-12 21:20:25 +0000

    Set up React Router

---
+++ src/pages/CartPage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const CartPage = () => {
+  return (
+    <div>
+      <h2>Please, buy something!</h2>
+    </div>
+  );
+};
+
+export default CartPage;
---
+++ src/pages/index.js
@@ -0,0 +1,5 @@
+import Home from "./HomePage";
+import Cart from "./CartPage";
+import Error from "./ErrorPage";
+
+export { Home, Cart, Error };
